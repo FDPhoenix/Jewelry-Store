c159e64b8e38d4b7238fb74385c74533467155cb
ThienHoang12345az <chauhoangthiencm@gmail.com>
2024-03-20 14:30:16 +0700

    change

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -9,7 +9,12 @@ import img6 from '../images/14promax.jpg'
 import img7 from '../images/15.jpg'
 import img8 from '../images/x.jpg'
 import img9 from '../images/se2022.jpg'
-
+import img10 from'../images/12.jpg'
+import img11 from'../images/Ip11.jpg'
+import img12 from'../images/iPhone 12Pro.jpg'
+import img13 from'../images/iPhone 12ProMax.jpg'
+import img14 from'../images/iPhone 11 Pro.jpg'
+import img15 from'../images/iPhone 11ProMax.jpg'
 const product = [
     {
         id: 1,
@@ -39,7 +44,89 @@ const product = [
         price: "1099.0"
     },
 
-    
+    {
+        id: 5,
+        image: img4,
+        name: "iPhone 14 ",
+        price: "1150.0"
+    },
+
+    {
+        id: 6,
+        image: img5,
+        name: "iPhone 14 Pro",
+        price: "1099.0"
+    },
+
+    {
+        id: 7,
+        image: img6,
+        name: "iPhone 14 Pro Max",
+        price: "1099.0"
+    },
+
+    {
+        id: 8,
+        image: img7,
+        name: "iPhone 15 ",
+        price: "5000.o"
+    },
+
+    {
+        id: 9,
+        image: img8,
+        name: "iPhone SE",
+        price: "399.99"
+    },
+
+    {
+        id: 10,
+        image: img9,
+        name: "SE(2022)",
+        price: "499.99"
+    },
+
+    {
+        id: 11,
+        image: img10,
+        name: "Iphone 12",
+        price: "5000.0"
+    },
+
+    {
+        id: 12,
+        image: img11,
+        name: "Iphone 11",
+        price: "5000.0"
+    },
+
+    {
+        id: 13,
+        image: img12,
+        name: "Iphone 12Pro",
+        price: "5120.0"
+    },
+
+    {
+        id: 14,
+        image: img13,
+        name: "Iphone 12ProMax",
+        price: "5120.0"
+    },
+
+    {
+        id: 15,
+        image: img14,
+        name: "Iphone 11 Pro",
+        price: "4902.0"
+    },
+
+    {
+        id: 16,
+        image: img15,
+        name: "Iphone 11 ProMax",
+        price: "5000.0"
+    },
 ];
 
 function Product() {
