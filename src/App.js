c3a6bf4d43013d026b48dc67f3df7bfee83b81da
FDPhoenix <khanghuynhfdp@gmail.com>
2024-03-20 14:37:07 +0700

    change

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/HomePage';
+import AboutPage from './Pages/AboutPage';
+import ContactPage from './Pages/ContactPage';
+import CartPage from './Pages/CartPage';
+import LoginPage from './Pages/LoginPage';
+import ProductPage from './Pages/ProductPage';
 
 function App() {
   return (
     <div>
-      <HomePage />
-
       <Routes>
         <Route path='/' element={<HomePage />} />
-        {/* <Route path='' element={}/>
-        <Route path='' element={}/>
-        <Route path='' element={}/>
-        <Route path='' element={}/> */}
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/cart' element={<CartPage />} />
+        <Route path='/product' element={<ProductPage />} />
       </Routes>
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
@@ -9,24 +9,23 @@ function Header() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container px-4 px-lg-5">
 
-          <a className="navbar-brand" href="Home.jsp">Home</a>
+          <Link className="navbar-brand" to='/'>Home</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-              <li class="nav-item"><a class="nav-link" href="#">About</a></li>
-              <li class="nav-item"><a class="nav-link" href="#">Product</a></li>
-              <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+              <li class="nav-item"><Link class="nav-link" to='/about'>About</Link></li>
+              <li class="nav-item"><Link class="nav-link" to='/product'>Product</Link></li>
+              <li class="nav-item"><Link class="nav-link" to='/contact'>Contact</Link></li>
             </ul>
 
             <form className="d-flex">
-              <button className="btn btn-outline-dark" type="submit">
-                <i className="bi-cart-fill me-1"></i>
+              <Link className="btn btn-outline-dark" to='/cart'>
                 Cart
                 <span className="badge bg-dark text-white ms-1 rounded-pill">0</span>
-              </button>
+              </Link>
               <button className="ms-2 btn btn-outline-dark">
-                <a className="nav-link active" aria-current="page" href="Login.jsp">Login</a>
+                <Link className="nav-link active" aria-current="page" to="/login">Login</Link>
               </button>
             </form>
           </div>
